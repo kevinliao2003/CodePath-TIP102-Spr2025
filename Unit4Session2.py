3355e354492925cb61cc8bd1d434187c6798a7a8
Kevin Liao <kliao@terpmail.umd.edu>
2025-03-07 15:16:06 -0500

    sessions 1 and 2

--- Unit4Session2.py
+++ Unit4Session2.py
@@ -32,3 +32,61 @@ track_scene_transitions(scenes)
 
 scenes = ["Introduction", "Conflict", "Climax", "Denouement"]
 track_scene_transitions(scenes)
+
+# Advanced Problem Set Version 2 - Problem 2: Find Most Wasted Food Item
+def find_most_wasted_food_item(waste_records):
+    """
+    U - Understand
+    - identify the food items that was wasted the most
+    - no lists will be empty
+
+    M - Match
+    - arrays
+
+    P - Plan
+    variables:
+    - res (most wasted current food)
+
+    loop through the records
+        get the total for the current food
+        update res if necessary
+    
+    return res
+
+    I - Implement
+    - see code below
+
+    R - Review
+    - see test cases
+
+    E - Evaluate
+    """
+
+    res = "" # most wasted food to return
+    max_waste = 0 # amount of most wasted food
+    for food, amounts in waste_records.items():
+        total = sum(amounts)
+        if total > max_waste:
+            res = food
+            max_waste = total
+
+    return res
+
+waste_records1 = {
+    "Apples": [200, 150, 50],
+    "Bananas": [100, 200, 50],
+    "Carrots": [150, 100, 200],
+    "Tomatoes": [50, 50, 50]
+}
+result = find_most_wasted_food_item(waste_records1)
+# print(result)
+assert result == "Carrots"
+
+waste_records2 = {
+    "Bread": [300, 400],
+    "Milk": [200, 150],
+    "Cheese": [100, 200, 100],
+    "Fruits": [400, 100]
+}
+result = find_most_wasted_food_item(waste_records2)
+assert result == "Bread"
