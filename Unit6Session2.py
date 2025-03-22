c3f194da1fe9292cac2496a7ed51ec8c5eec6c23
Kevin Liao <kliao@terpmail.umd.edu>
2025-03-22 07:34:51 -0400

    unit 6 session 2

--- Unit6Session2.py
+++ Unit6Session2.py
@@ -1,3 +1,5 @@
+import random
+
 # Advanced Problem Set Version 1 - Problem 4: Shared Music Taste
 class Node:
 	def __init__(self, value, next=None):
@@ -71,3 +73,56 @@ playlist_a.next.next = shared_segment
 playlist_b.next.next.next = shared_segment
 res = (playlist_overlap(playlist_a, playlist_b)).value
 assert res == 'Song M'
+
+# Advanced Problem Set Version 2 - Problem 2: Surprise Me
+def get_random(playlist):
+    """
+    U - Understand
+    - return a random node
+    - each node must have the same probability of being chosen
+    - there is at least 1 node in the list
+
+    M - Match
+    - linked list
+
+    P - Plan
+    variables:
+    - count (number of nodes in the list)
+    - curr (current node)
+    - res (result node to return)
+
+    while curr
+        get a random number from 0 to count
+        if it's 0
+            set the res to the current node
+        move curr to the right
+        increment count
+
+    return res
+
+    I - Implement
+    - see code below
+
+    R - Review
+    - see test cases below
+
+    E - Evaluate
+    - TC: O(n) where n is the number of nodes in the list
+    - SC: O(1)
+    """
+
+    res, curr = None, playlist
+    count = 0
+    while curr:
+        if random.randint(0, count) == 0:
+            res = curr.value
+        curr = curr.next
+        count += 1
+    
+    return res
+
+catalogue = Node(('Homegoing', 'Yaa Gyasi'), 
+                Node(('Pachinko', 'Min Jin Lee'),
+                         Node(('The Night Watchman', 'Louise Erdrich'))))
+
+print(get_random(catalogue))
