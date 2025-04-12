a90add4857db7a247f2c91b7b809367ba191c1b9
Kevin Liao <kliao@terpmail.umd.edu>
2025-04-12 07:48:46 -0400

    unit 9 session 1

--- Unit9Session1.py
+++ Unit9Session1.py
@@ -1,12 +1,29 @@
 from collections import deque
 
-# Advanced Problem Set Version 1 - Problem 4: Find Next Order to Fulfill Today
 class TreeNode():
      def __init__(self, order, left=None, right=None):
         self.val = order
         self.left = left
         self.right = right
 
+def print_tree(root):
+    if not root:
+        return "Empty"
+    result = []
+    queue = deque([root])
+    while queue:
+        node = queue.popleft()
+        if node:
+            result.append(node.val)
+            queue.append(node.left)
+            queue.append(node.right)
+        else:
+            result.append(None)
+    while result and result[-1] is None:
+        result.pop()
+    print(result)
+
+# Advanced Problem Set Version 1 - Problem 4: Find Next Order to Fulfill Today
 def larger_order_tree(order_tree, order):
     """
     U - Understand
@@ -85,3 +102,53 @@ assert next_order1.val == "Eclair"
 assert next_order2 == None
 
 # Advanced Problem Set Version 2 - Problem 2: Reverse Odd Levels of the Hotel
+def reverse_odd_levels(hotel):
+    """
+    U - Understand
+    - reverse the node values at each odd level
+    - the level starts at 0, with the root
+
+    M - Match
+    - dfs
+
+    P - Plan
+    create a dfs helper function
+        parameters:
+            - level (the current level)
+            - curr (current node)
+
+        if the curr is not valid
+            return
+
+        if the level is even
+            reverse the node values
+        
+        call dfs on left subtree, updating the current level
+        call dfs on right subtree, updating the current level
+
+    I - Implement
+    - see code below
+
+    R - Review
+    - see test cases below
+
+    E - Evaluate
+    """
+
+    def dfs(curr, level):
+        if not curr:
+            return
+        
+        if level % 2 == 0 and curr.left and curr.right:
+            curr.left.val, curr.right.val = curr.right.val, curr.left.val
+        
+        dfs(curr.left, level + 1)
+        dfs(curr.right, level + 1)
+
+    dfs(hotel, 0)
+    return hotel
+
+hotel = TreeNode("Lobby", 
+            TreeNode(102, TreeNode(201), TreeNode(202)), 
+                TreeNode(101, TreeNode(203), TreeNode(204)))
+print_tree(reverse_odd_levels(hotel))
